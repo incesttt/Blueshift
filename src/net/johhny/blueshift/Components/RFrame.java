94b690d203871c1211af9ca5cad414eecfa0f952
johhnyhax <igoranonim1337@gmail.com>
2017-02-06 20:43:18 +0000

    Updated Sourcecode

---
+++ src/net/johhny/blueshift/Components/RFrame.java
@@ -0,0 +1,13 @@
+package net.johhny.blueshift.Components;
+
+import javax.swing.*;
+
+/**
+ * Created by johhnyhax
+ *     on 06.02.17.
+ */
+public class RFrame extends JFrame{
+    public RFrame(){
+        super("RFrame");
+    }
+}
---
+++ src/net/johhny/blueshift/Start.java
@@ -0,0 +1,8 @@
+package net.johhny.blueshift;
+
+/**
+ * Created by johhnyhax
+ *     on 06.02.17.
+ */
+public class Start {
+}
