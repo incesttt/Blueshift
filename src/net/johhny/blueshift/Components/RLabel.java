4019b954137b61b12b5aa6af3ad3f120b030ddcf
johhnyhax <igoranonim1337@gmail.com>
2017-02-08 20:20:12 +0000

    RLabel is now ready to basic use.

--- src/net/johhny/blueshift/Components/RLabel.java
+++ src/net/johhny/blueshift/Components/RLabel.java
@@ -1,6 +1,7 @@
 package net.johhny.blueshift.Components;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by johhnyhax
@@ -8,5 +9,54 @@ import javax.swing.*;
  */
 public class RLabel extends JLabel{
 
+    private int labelX,labelY,labelWidth,labelHeight;
+    private String labelTxt = "";
+    private Color ForegroundColor,backgroundColor;
+
+    public RLabel(int x, int y, int width, int height)
+    {
+        this.labelX = x;
+        this.labelY = y;
+        this.labelWidth = width;
+        this.labelHeight = height;
+        this.setBounds(labelX,labelY,labelWidth,labelHeight);
+    }
+
+    public RLabel(String labelTxt1,int labelWidth, int labelHeight, int x, int y)
+    {
+        this.labelTxt = labelTxt1;
+        this.setText(labelTxt);
+        this.labelX = x;
+        this.labelY = y;
+        this.labelWidth = labelWidth;
+        this.labelHeight = labelHeight;
+    }
+
+    public RLabel(String labelTxt1,Color foregroundColor,Color backgroundColor,int labelWidth, int labelHeight, int x, int y)
+    {
+        this.labelTxt = labelTxt1;
+        this.setForeground(foregroundColor);
+        this.setBackground(backgroundColor);
+        this.labelWidth = labelWidth;
+        this.labelHeight = labelHeight;
+        this.labelX = x;
+        this.labelY = y;
+    }
+
+    public void setText(String text)
+    {
+        super.setText(text);
+        labelTxt = text;
+    }
+
+
+    public void setBounds(int x, int y, int width, int height)
+    {
+        super.setBounds(x,y,width,height);
+        this.labelX = x;
+        this.labelY = y;
+        this.labelWidth = width;
+        this.labelHeight = height;
+    }
 
 }
