bd52a303190490d673abcd149f2fb361a64f8e02
johhnyhax <igoranonim1337@gmail.com>
2017-02-08 20:24:01 +0000

    RTextField is now ready to basic use.

--- src/net/johhny/blueshift/Components/RTextField.java
+++ src/net/johhny/blueshift/Components/RTextField.java
@@ -7,5 +7,42 @@ import javax.swing.*;
  *     on 06.02.17.
  */
 public class RTextField extends JTextField{
+    private int TextFieldX,TextFieldY,TextFieldWidth,TextFieldHeight;
+    private String TextFieldTxt = "";
 
+    public RTextField(int x, int y, int width, int height)
+    {
+        this.TextFieldX = x;
+        this.TextFieldY = y;
+        this.TextFieldWidth = width;
+        this.TextFieldHeight = height;
+        this.setBounds(TextFieldX,TextFieldY,TextFieldWidth,TextFieldHeight);
+    }
+
+    public RTextField(String TextFieldTxt1,int TextFieldWidth, int TextFieldHeight, int x, int y)
+    {
+        this.TextFieldTxt = TextFieldTxt1;
+        this.setText(TextFieldTxt);
+        this.TextFieldX = x;
+        this.TextFieldY = y;
+        this.TextFieldWidth = TextFieldWidth;
+        this.TextFieldHeight = TextFieldHeight;
+    }
+
+    public void setText(String text)
+    {
+        super.setText(text);
+        TextFieldTxt = text;
+    }
+
+
+    public void setBounds(int x, int y, int width, int height)
+    {
+        super.setBounds(x,y,width,height);
+        this.TextFieldX = x;
+        this.TextFieldY = y;
+        this.TextFieldWidth = width;
+        this.TextFieldHeight = height;
+    }
+    
 }
